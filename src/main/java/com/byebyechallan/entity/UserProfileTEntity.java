2dd47c0ebab109f2deb46ed022685a8a126c62ef
vivekwork01 <vivek.knit69@gmail.com>
2026-08-09 00:21:30 +0530

    fixing refresh issue of the My Profile page

--- src/main/java/com/byebyechallan/entity/UserProfileTEntity.java
+++ src/main/java/com/byebyechallan/entity/UserProfileTEntity.java
@@ -17,9 +17,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "user_profile_t")
 
 @NamedNativeQuery(name = "UserProfileTEntity.getAllProfile",
-    query = "SELECT pt.id id, pt.user_id user_id, pt.name profile_name, COUNT(DISTINCT dt.vehicle_registration_no) vehicle_count "
+    query = "SELECT pt.id id, pt.user_id user_id, pt.name profile_name, COUNT(DISTINCT pvt.id) vehicle_count "
         + "FROM user_profile_t pt "
-        + "LEFT JOIN user_document_t dt ON dt.profile_id=pt.id AND pt.is_deleted=0 and dt.is_deleted=0 "
+        + "LEFT JOIN core_profile_vehicle_tr pvt ON pvt.profile_id=pt.id AND pt.is_deleted=0 and pvt.is_deleted=0 "
         + "where pt.user_id=:userId GROUP BY pt.id",
     resultClass = ProfileDto.class)
 public class UserProfileTEntity {
